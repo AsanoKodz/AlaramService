cc17f485d6724202e3b3ce836d0e7e8668cda77c
AsanoKodz <isip_a.v.doduhova@mpt.ru>
2022-06-19 23:02:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alaramservice/AlarmReceiver.java
@@ -0,0 +1,20 @@
+package com.example.alaramservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Toast.makeText(context, "Будильник!!!", Toast.LENGTH_SHORT).show();
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(1000);
+
+
+    }
+}
